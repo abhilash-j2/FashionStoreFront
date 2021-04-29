f47f93215bbd7bd429724d41faf680984ed7e123
Devanathan Sabapathy <sdevanathan95@gmail.com>
2021-04-28 21:41:58 -0400

    UI files for login and registeration #10

---
+++ webapp/Fashion_recommendation_store_login_py_files/__init__.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'f7c35f1e4e44ddedde3125e5'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = 'info'
+
+from app import routes
+
+
+# @app.route('/about/<username>')
+# def about_page(username):
+#     return f'<h2>This is the about page of {username}</h2>'
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
---
+++ webapp/Fashion_recommendation_store_login_py_files/routes.py
@@ -0,0 +1,60 @@
+from app import app
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
+from app.module import Item
+from app.forms import RegisterForm, LoginForm
+from app.module import Item,User
+from app import db
+from flask_login import login_user, logout_user, login_required
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+@login_required
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html',items=items)
+
+@app.route('/register', methods=['GET','POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                                email_address=form.email_address.data,
+                                password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+
+        login_user(user_to_create)
+        flash(f'Account created successfully! You are now logged in as  {user_to_create.username}', category='success')
+        return redirect(url_for('market_page'))
+    if form.errors != {}: # if there are no errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
+    return render_template('register.html',form = form)
+
+@app.route('/login',methods=['GET','POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password1.data):
+            login_user(attempted_user)
+            flash(f'Success! you are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+
+        else:
+            flash('Username and Password are not a match! Please try again', category='danger')
+    return render_template('login.html',form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash('You have been logged out!', category='info')
+    return redirect(url_for('home_page'))
+
+
+
